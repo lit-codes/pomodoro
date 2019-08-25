543c3d970d3e9de471d0553aff64e7db73baa532
Lit Koder <litkoder@gmail.com>
2019-08-25 14:05:11 +0800

    Remove irrelevant comments and the annoying loop

--- backend/files/src/send_notification.js
+++ backend/files/src/send_notification.js
@@ -8,15 +8,10 @@ const messaging = new FirebaseMessaging({
 
 process.argv.slice(2).forEach(client => messaging.addClient(client));
 
-// Send a message to the device corresponding to the provided
-// registration token.
-setInterval(() => {
-  messaging.send({ key: 'value' })
-    .then((responses) => {
-      // Response is a message ID string.
-      console.log('Successfully sent message:', responses);
-    })
-    .catch((error) => {
-      console.log('Error sending message:', error);
-    });
-}, 4000);
+messaging.send({ key: 'value' })
+  .then((responses) => {
+    console.log('Successfully sent message:', responses);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
